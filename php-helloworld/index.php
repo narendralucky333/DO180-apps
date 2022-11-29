80b576bd379f9ed50a266f333037e9fc25281fe9
Student User <student@workstation.lab.example.com>
2022-11-28 23:55:52 -0500

    updataed lines

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,8 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, this is Narendra Lucky " . PHP_VERSION . "\n";
-print "I started work at IMSS " . PHP_VERSION . "\n";
+<br>
+print "Hello, this is Narendra Lucky " . "\n";
+<br>
+print "I started work at IMSS " . "\n";
 ?>
 
